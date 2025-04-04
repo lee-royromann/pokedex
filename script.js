235a4618968e27dd13aa63793b887a316eb817ba
Lee-Roy Romann <104465642+lee-royromann@users.noreply.github.com>
2025-04-04 05:03:59 +0200

    feat: added init function with API call and loading spinner overlay

---
+++ script.js
@@ -0,0 +1,32 @@
+let offset = 0;
+let limit = 12;
+let totalPokemonCount = 0;
+let pokemonData = []; // url + name
+
+function init() {
+    getPokemonURLs();
+}
+
+async function getPokemonURLs() {
+    showLoadingOverlay();
+    try {
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);        
+        pokemonData = await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+    finally {
+        hideLoadingOverlay();
+        console.log(pokemonData);
+    }
+}
+
+function showLoadingOverlay() {
+    document.getElementById("overlay__spinner").classList.remove("d_none");
+    document.body.classList.add("overflow-hidden");
+}
+
+function hideLoadingOverlay() {
+    document.getElementById("overlay__spinner").classList.add("d_none");
+    document.body.classList.remove("overflow-hidden");
+}
